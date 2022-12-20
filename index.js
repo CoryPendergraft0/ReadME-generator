d4d953f641cdc43883c60fba0e8c7f05d3a1624d
Cory Pendergraft <cory.pendergraft81@gmail.com>
2022-12-19 19:05:01 -0500

    aded more files, inquirer, and package

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the name of your New Project?',
+            name: 'Title of Project'
+        },   
+        {
+            type: 'input',
+            message: 'Description of your Project?',
+            name: 'Description',
+        },
+        {
+            type: 
+        },
+
+
+
+
+    ])
