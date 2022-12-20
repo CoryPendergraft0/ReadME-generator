3733ccb104abb0ee5df93bd593f23cb1fb513130
Cory Pendergraft <cory.pendergraft81@gmail.com>
2022-12-19 19:49:06 -0500

    added more inputs and made the page generate

--- index.js
+++ index.js
@@ -3,21 +3,54 @@ const fs = require('fs');
 
 inquirer
     .prompt([
+       {
+            type: 'input',
+            name: 'github',
+            message: 'What is your Github Username?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Everyone has a username, whats yours?');
+                    return false;
+                }
+            }
+       },
         {
             type: 'input',
+            name: 'Title of Project',
             message: 'What is the name of your New Project?',
-            name: 'Title of Project'
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Every Project has a name silly goose!');
+                    return false;
+                }
+            }
         },   
         {
             type: 'input',
-            message: 'Description of your Project?',
             name: 'Description',
+            message: 'Write a Quick Description of your Project',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Come on... Nothing to tell me about it?')
+                }
+            }
         },
         {
-            type: 
-        },
+            type: 'input',
+            
+        }
 
 
 
 
     ])
+
+.then(answers => {
+    return generatePage(answers);
+})
